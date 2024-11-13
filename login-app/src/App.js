2fd021250bd882e2728c79b6db8886f4a56a5c63
laurazap1 <zapatalcollege@gmail.com>
2024-11-13 16:21:14 -0500

    Test git commands

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -5,7 +5,7 @@ import LoginPage from './components/LoginPage';
 function App() {
   return (
     <div className="App">
-      <h1>Login Page</h1>
+      <h1>Login</h1>
       <LoginPage />
     </div>
   );
